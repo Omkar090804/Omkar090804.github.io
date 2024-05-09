9927e849d262191831ee1875ea151d484dfdb55b
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-05-09 16:34:50 +0530

    Add files via upload

---
+++ Resume/script.js
@@ -0,0 +1,10 @@
+<a href="https://linkedin.com/in/omkar darekar" target="_blank"><img src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="LinkedIn" height="50" width="50" /></a>
+    <span style="margin-right: 20px;"></span> <!-- Adjust the margin-right value as needed -->
+    <a href="https://www.hackerrank.com/@omkardarekar0908" target="_blank"><img src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/hackerrank.svg" alt="HackerRank" height="50" width="50" /></a>
+    <span style="margin-right: 20px;"></span> <!-- Adjust the margin-right value as needed -->
+    <a href="https://www.leetcode.com/omkar_0908" target="_blank"><img src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="LeetCode" height="50" width="50" /></a>
+    <span style="margin-right: 20px;"></span> <!-- Adjust the margin-right value as needed -->
+    <a href="https://github.com/" target="_blank"><img src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="GitHub" height="50" width="50" /></a>
+
+    <span style="margin-right: 20px;"></span> <!-- Adjust the margin-right value as needed -->
+    <a href="https://tr.ee/EwqWsdDure" target="_blank"><img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/microsoftlearn.svg" alt="Microsoft Learn" height="50" width="50" /></a>
